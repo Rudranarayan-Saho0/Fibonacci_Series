91b6568acff3bbdab33e0aad3362e0e51f295cf6
Rudranarayan Sahoo <175521447+Rudranarayan-Saho0@users.noreply.github.com>
2024-07-21 10:14:54 +0530

    FibonacciSeries.py

--- FibonacciSeries.py
+++ FibonacciSeries.py
@@ -4,14 +4,13 @@ x = 0
 y = 1
 z = 0
 
-print(f"fibonacci series of 0 is {x}")
-print(f"fibonacci series of 1 is {y}")
+print(f"fibonacci series of 1 is {x}")
+print(f"fibonacci series of 2 is {y}")
 
 i = 1
-while i<n:
+while i<n-1:
     z = x+y
     x = y 
     y = z
     i+=1
-    print(f"fibonacci series of {i} is {z}")
-
+    print(f"fibonacci series of {i+1} is {z}")
